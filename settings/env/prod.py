fb6cc2af311075e42b85e22eb74b57569bfedc43
Nargiz <nnadirbek@gmail.com>
2023-06-15 21:23:57 +0600

    Updated db configuration

--- settings/env/prod.py
+++ settings/env/prod.py
@@ -10,8 +10,12 @@ ASGI_APPLICATION = 'deploy.prod.asgi.application'
 #
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dtalpu6hl0c5j',
+        'USER': 'ovfcpsfpknvmxc',
+        'PASSWORD': '0dc04a3857bd803bb3d7d11480c3f6bf360daee45a21a9f21ba75c255ce29adf',  # noqa
+        'HOST': 'ec2-44-195-132-31.compute-1.amazonaws.com',
+        'PORT': 5432
     }
 }
 ALLOWED_HOSTS = []
